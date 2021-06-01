4f850bd8f62f465bff281ae8dbc0d85e9e939df3
ewrt <ewrt101@gmail.com>
2021-06-02 02:30:50 +1200

    hmmmm

--- js/menu.js
+++ js/menu.js
@@ -1,26 +1,21 @@
 const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-const navLink = document.querySelectorAll(".nav-link");
-//const header = Document.querySelector(".header");
+
 var menu = 0;
 hamburger.addEventListener("click", mobileMenu);
-navLink.forEach(n => n.addEventListener("click", closeMenu));
+
 
 function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+
     if (menu == 0){
         document.getElementById("header").style.height = "100vh";
+        document.getElementById("header").style.height = "100vh";
+        document.getElementById("menu").style.display = "flex";
         menu = 1;
     }else {
         document.getElementById("header").style.height = "5vh";
+        document.getElementById("menu").style.display = "none";
         menu = 0;
     }
     
     console.log("Test");
-}
-
-function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
 }
